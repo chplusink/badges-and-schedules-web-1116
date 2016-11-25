3b1a35fd9df533bf18935bc9dd0ff89d47348f6c
Vic Lam <lam.vic.k@gmail.com>
2016-11-25 19:44:56 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,30 @@
-# Write your code here.
+require 'pry'
+
+def badge_maker(nombre)
+  "Hello, my name is #{nombre}."
+end
+
+def batch_badge_creator(conferenciantes)
+  insignias = []
+  conferenciantes.each_with_index do |nombre, indice|
+    insignias[indice] = "Hello, my name is #{nombre}."
+  end
+  insignias
+end
+
+def assign_rooms(conferenciantes)
+  encargos = []
+  conferenciantes.each_with_index do |nombre, indice|
+    encargos[indice] = "Hello, #{nombre}! You'll be assigned to room #{indice+1}!"
+  end
+  encargos
+end
+
+def printer(conferenciantes)
+  batch_badge_creator(conferenciantes).each do |elemento|
+    puts elemento
+  end
+  assign_rooms(conferenciantes).each do |elemento|
+    puts elemento
+  end
+end
